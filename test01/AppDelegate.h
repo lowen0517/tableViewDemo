f4822c59326429c78f823300139afed8a248800b
fisher xia <xy@fisher-xiademacbook-air.local>
2016-08-16 14:35:36 +0800

    Initial Commit

---
+++ test01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test01
+//
+//  Created by fisher_xia on 16/8/16.
+//  Copyright © 2016年 fisher_xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test01
+//
+//  Created by fisher_xia on 16/8/16.
+//  Copyright © 2016年 fisher_xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
